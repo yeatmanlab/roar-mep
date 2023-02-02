3a5dc3783e06d58dfff04c71243f34b9bc80b457
Adam Richie-Halford <richiehalford@gmail.com>
2023-02-02 05:49:29 -0800

    Add precue videos

--- src/videos.js
+++ src/videos.js
@@ -35,52 +35,132 @@ const clickStarTrial = {
   margin_horizontal: "inherit",
 };
 
-const videoPrefix = config.pseudoFont ? "pseudo" : "latin";
+let videoTrials;
 
 /* define instructions trial */
-const introTrial = [{
-  stimulus: [videoContent[`${videoPrefix}Intro`]],
-  ...kwargs,
-}, clickStarTrial];
+if (config.precue) {
+  const introTrial = [{
+    stimulus: [videoContent.intro],
+    ...kwargs,
+  }, clickStarTrial];
 
-const postPracticeTrial = [{
-  stimulus: [videoContent[`${videoPrefix}PostPractice`]],
-  ...kwargs,
-}, clickStarTrial];
+  const postPracticeTrial = [{
+    stimulus: [videoContent.postPractice],
+    ...kwargs,
+  }, clickStarTrial];
 
-const postTwoLetterBlockTrial = [{
-  stimulus: [videoContent[`${videoPrefix}Post2Block`]],
-  ...kwargs,
-}, clickStarTrial];
+  const midBlock1Trial = [{
+    stimulus: [videoContent.midBlock1],
+    ...kwargs,
+  }, clickStarTrial];
 
-const postBlock1Trial = [{
-  stimulus: [videoContent[`${videoPrefix}Block1`]],
-  ...kwargs,
-}, clickStarTrial];
+  const postBlock1Trial = [{
+    stimulus: [videoContent.postBlock1],
+    ...kwargs,
+  }, clickStarTrial];
 
-const rewardAnimation1Trial = [{
-  stimulus: [videoContent[`${videoPrefix}RewardAnimation1`]],
-  ...kwargs,
-}, clickStarTrial];
+  const midBlock2Trial = [{
+    stimulus: [videoContent.midBlock2],
+    ...kwargs,
+  }, clickStarTrial];
 
-const rewardAnimation2Trial = [{
-  stimulus: [videoContent[`${videoPrefix}RewardAnimation2`]],
-  ...kwargs,
-}, clickStarTrial];
+  const postBlock2Trial = [{
+    stimulus: [videoContent.postBlock2],
+    ...kwargs,
+  }, clickStarTrial];
 
-const endTrial = [{
-  stimulus: [videoContent[`${videoPrefix}End`]],
-  ...kwargs,
-}, clickStarTrial];
+  const midBlock3Trial = [{
+    stimulus: [videoContent.midBlock3],
+    ...kwargs,
+  }, clickStarTrial];
 
-const videoTrials = {
-  intro: introTrial,
-  postPractice: postPracticeTrial,
-  postTwoLetterBlock: postTwoLetterBlockTrial,
-  postBlock1: postBlock1Trial,
-  rewardAnimation1: rewardAnimation1Trial,
-  rewardAnimation2: rewardAnimation2Trial,
-  end: endTrial,
-};
+  const postBlock3Trial = [{
+    stimulus: [videoContent.postBlock3],
+    ...kwargs,
+  }, clickStarTrial];
 
-export default videoTrials;
+  const midBlock4Trial = [{
+    stimulus: [videoContent.midBlock4],
+    ...kwargs,
+  }, clickStarTrial];
+
+  const postBlock4Trial = [{
+    stimulus: [videoContent.postBlock4],
+    ...kwargs,
+  }, clickStarTrial];
+
+  const midBlock5Trial = [{
+    stimulus: [videoContent.midBlock5],
+    ...kwargs,
+  }, clickStarTrial];
+
+  const endTrial = [{
+    stimulus: [videoContent.end],
+    ...kwargs,
+  }, clickStarTrial];
+
+  videoTrials = {
+    intro: introTrial,
+    postPractice: postPracticeTrial,
+    midBlock1: midBlock1Trial,
+    postBlock1: postBlock1Trial,
+    midBlock2: midBlock2Trial,
+    postBlock2: postBlock2Trial,
+    midBlock3: midBlock3Trial,
+    postBlock3: postBlock3Trial,
+    midBlock4: midBlock4Trial,
+    postBlock4: postBlock4Trial,
+    midBlock5: midBlock5Trial,
+    end: endTrial,
+  };
+} else {
+  const videoPrefix = config.pseudoFont ? "pseudo" : "latin";
+
+  const introTrial = [{
+    stimulus: [videoContent[`${videoPrefix}Intro`]],
+    ...kwargs,
+  }, clickStarTrial];
+
+  const postPracticeTrial = [{
+    stimulus: [videoContent[`${videoPrefix}PostPractice`]],
+    ...kwargs,
+  }, clickStarTrial];
+
+  const postTwoLetterBlockTrial = [{
+    stimulus: [videoContent[`${videoPrefix}Post2Block`]],
+    ...kwargs,
+  }, clickStarTrial];
+
+  const postBlock1Trial = [{
+    stimulus: [videoContent[`${videoPrefix}Block1`]],
+    ...kwargs,
+  }, clickStarTrial];
+
+  const rewardAnimation1Trial = [{
+    stimulus: [videoContent[`${videoPrefix}RewardAnimation1`]],
+    ...kwargs,
+  }, clickStarTrial];
+
+  const rewardAnimation2Trial = [{
+    stimulus: [videoContent[`${videoPrefix}RewardAnimation2`]],
+    ...kwargs,
+  }, clickStarTrial];
+
+  const endTrial = [{
+    stimulus: [videoContent[`${videoPrefix}End`]],
+    ...kwargs,
+  }, clickStarTrial];
+
+  videoTrials = {
+    intro: introTrial,
+    postPractice: postPracticeTrial,
+    postTwoLetterBlock: postTwoLetterBlockTrial,
+    postBlock1: postBlock1Trial,
+    rewardAnimation1: rewardAnimation1Trial,
+    rewardAnimation2: rewardAnimation2Trial,
+    end: endTrial,
+  };
+}
+
+const videoExports = videoTrials;
+export default videoExports;
